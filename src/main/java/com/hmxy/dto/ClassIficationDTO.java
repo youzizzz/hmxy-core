060b1c23c6de7d2271d9efd9b84d321d2609ca9e
唐由之 <tangyz@tongc-soft.com>
2018-11-06 17:57:12 +0800

    JackJson日期序列化支持

--- src/main/java/com/hmxy/dto/ClassIficationDTO.java
+++ src/main/java/com/hmxy/dto/ClassIficationDTO.java
@@ -1,5 +1,6 @@
 package com.hmxy.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -10,9 +11,13 @@ import java.util.Date;
  */
 @Data
 public class ClassIficationDTO implements Serializable{
+
     private String  creatorBy;
+
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
     private Date creatorDate;
     private String  updateBy;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
     private Date  updateDate;
     private String  cfId;
     private String  categoryName;
